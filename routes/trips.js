9484db2dba52818693bc157d99f3508a146288dd
Eva Jeliazkova <35533530+ejeliazkova@users.noreply.github.com>
2025-03-04 16:06:52 -0600

    add cors and change server port

--- routes/trips.js
+++ routes/trips.js
@@ -11,4 +11,17 @@ router.get('/', async function(req, res, next) {
         next(err);
     }
 });
+
+// TODO: Our post to create a new trip. The json that's returned should contain a link to the new trip i.e. 
+// returns the TripId of the new trip so the user can see
+router.post('/', async function(req, res, next) {
+    try {
+        // Extract the pertinent info from req to pass as an object to createTrip
+        // Refer to express docs to see how to pass the necessary info from req
+        res.json(await currentTrips.createTrip());
+    } catch (err) {
+        console.error(`Error while getting current trips `, err.message);
+        next(err);
+    }
+});
 module.exports = router;
--- services/trips.js
+++ services/trips.js
@@ -7,7 +7,7 @@ async function getMultiple(page = 1){
     
     const rows = await db.query(
         `SELECT id, name, userId 
-    FROM trips LIMIT ${offset},${config.listPerPage}`
+        FROM trips LIMIT ${offset},${config.listPerPage}`
     );
     const data = helper.emptyOrRows(rows);
     const meta = {page};
@@ -19,9 +19,10 @@ async function getMultiple(page = 1){
     // return {"data":[{"id":1,"name":"JavaScript","released_year":1995,"githut_rank":1,"pypl_rank":3,"tiobe_rank":7},{"id":2,"name":"Python","released_year":1991,"githut_rank":2,"pypl_rank":1,"tiobe_rank":3},{"id":3,"name":"Java","released_year":1995,"githut_rank":3,"pypl_rank":2,"tiobe_rank":2},{"id":4,"name":"TypeScript","released_year":2012,"githut_rank":7,"pypl_rank":10,"tiobe_rank":42},{"id":5,"name":"C#","released_year":2000,"githut_rank":9,"pypl_rank":4,"tiobe_rank":5},{"id":6,"name":"PHP","released_year":1995,"githut_rank":8,"pypl_rank":6,"tiobe_rank":8},{"id":7,"name":"C++","released_year":1985,"githut_rank":5,"pypl_rank":5,"tiobe_rank":4},{"id":8,"name":"C","released_year":1972,"githut_rank":10,"pypl_rank":5,"tiobe_rank":1},{"id":9,"name":"Ruby","released_year":1995,"githut_rank":6,"pypl_rank":15,"tiobe_rank":15},{"id":10,"name":"R","released_year":1993,"githut_rank":33,"pypl_rank":7,"tiobe_rank":9}],"meta":{"page":1}}
 }
 
-async function getTripsForUser($userId){
+async function createTrip(){
     const offset = helper.getOffset(page, config.listPerPage);
     
+    // TODO: We'll need to use an insert statement 
     const rows = await db.query(
         `SELECT id, name, userId 
         FROM trips LIMIT ${offset},${config.listPerPage}
@@ -36,6 +37,5 @@ async function getTripsForUser($userId){
 }
 
 module.exports = {
-    getMultiple,
-    getTripsForUser
+    getMultiple
 }
