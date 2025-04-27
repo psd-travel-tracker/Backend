14211a425c893c3be3278d0925bb68f71044cb8b
Eva Jeliazkova <35533530+ejeliazkova@users.noreply.github.com>
2025-04-27 16:07:49 -0500

    addign expenses route and service definition

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const expensesRouter = require('./routes/expenses');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const tripsRouter = require('./routes/trips');
+var usersRouter = require('./routes/users');
 
 
 var app = express();
@@ -17,15 +18,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/expenses', expensesRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/trips', tripsRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
