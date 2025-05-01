337d81b0daf2ddcbd0659b384648a06f8e4c1dbd
Eva Jeliazkova <35533530+ejeliazkova@users.noreply.github.com>
2025-05-01 06:05:59 -0500

    user validation function, bcrypt, and indent on code in expenses

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var cors = require('cors');
+const bcrypt = require('bcrypt');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -18,11 +19,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const users = require('../services/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* check user */
+router.post('/', async function(req, res, next) {
+  try {
+    let userData = req.body;
+    
+    const result = await users.validateUser(userData);
+    if (result.error) {
+        return res.status(400).json(result);
+    }
+    // Return success response
+    res.status(201).json(result);
+  } catch (err) {
+      console.error(`Error while logging in `, err.message);
+      next(err);
+  }
 });
 
 module.exports = router;
